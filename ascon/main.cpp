8866870cd6e3215b6c8912f7b62c48f1a2fada3c
ilemeire <Isar.Lemeire@UGent.be>
2022-09-13 17:20:02 +0930

    finalising loading elements

--- ascon/main.cpp
+++ ascon/main.cpp
@@ -24,7 +24,7 @@ string IntToBinaryString(unsigned long long int input) {
 }
 
 
-
+//"x64/Debug/ascon.exe" -k abc -N 10 -A blabla -P test -debug 
 //inputs: K = key (max 160 bits), n = nonce (public message number, 128 bits), A= associated data, P = plaintext
 //Outputs: C = ciphertext, T = authentication tag (128 bits)
 int main(int argc, char* argv[])
@@ -34,6 +34,8 @@ int main(int argc, char* argv[])
     string nonce = "";
     string data = "";
     string plaintext = "";
+    int a = 12;
+    int b = 8;
     bool debug = false;
 
     //Load inputs
@@ -41,11 +43,14 @@ int main(int argc, char* argv[])
     {
         if (string(argv[i]) == "-h" || string(argv[i]) == "--help")
         {
-            cout << "-k or -key\tan acsii representation of the key consisting of maximum 20 characters" << endl;
-            cout << "-N or -nonce\ta decimal representation of the nonce or message number consisting of maximum 128 bits" << endl;
-            cout << "-A or -data\tan acsii representation of the associated data" << endl;
-            cout << "-P or -plaintext\tan acsii representation of the plaintext" << endl;
-            cout << "-debug\tturns on debug-mode" << endl;
+            cout << "-k or -key, an acsii representation of the key consisting of maximum 20 characters, must be given" << endl;
+            cout << "-N or -nonce, a decimal representation of the nonce or message number consisting of maximum 128 bits, must be given" << endl;
+            cout << "-A or -data, an acsii representation of the associated data" << endl;
+            cout << "-P or -plaintext, an acsii representation of the plaintext, must be given" << endl;
+            cout << "-a, finalisation permutation a, automatically set to 12" << endl;
+            cout << "-b, finalisation permutation a, automatically set to 8" << endl;
+            cout << "-debug, turns on debug-mode" << endl;
+            return 0;
         }
         else if (string(argv[i]) == "-k" || string(argv[i]) == "-key")
         {
@@ -61,11 +66,27 @@ int main(int argc, char* argv[])
         {
             unsigned long long int n = std::strtoull(argv[i+1], NULL, 0);
             if (n == NULL) {
-                cout << "nonce " << argv[i + 1] << " is not a valid decimal number" << endl;
+                cout << "nonce " << argv[i + 1] << " is not a valid decimal number bigger than 0" << endl;
                 return 0;
             }
             for (int j = 0; j < strlen(argv[i + 1]); j++) {
                 nonce = IntToBinaryString(n);
+            }
+        }
+        else if (string(argv[i]) == "-a")
+        {
+            a = std::strtoull(argv[i + 1], NULL, 0);
+            if (a == NULL) {
+                cout << "-a " << argv[i + 1] << " is not a valid decimal number bigger than 0" << endl;
+                return 0;
+            }
+        }
+        else if (string(argv[i]) == "-b")
+        {
+            b = std::strtoull(argv[i + 1], NULL, 0);
+            if (b == NULL) {
+                cout << "-b " << argv[i + 1] << " is not a valid decimal number bigger than 0" << endl;
+                return 0;
             }
         }
         else if (string(argv[i]) == "-A" || string(argv[i]) == "-data")
@@ -103,6 +124,8 @@ int main(int argc, char* argv[])
         cout << "nonce = " << nonce << endl;
         cout << "data = " << data << endl;
         cout << "plaintext = " << plaintext << endl;
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
     }
 
     
